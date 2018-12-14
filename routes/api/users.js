a0290a1b12bd3f41cee57d11d1f303fb747a41ba
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-12-14 13:33:40 -0700

    Building Auth/Login for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 
-
+// Getting User Model from Users.js
 const User = require('../../models/Users');
 
-//load model
-//api/users/register
-
+//Test route Will Remove
 router.get('/test', (req, res) => {
   res.json({
     msg: "users works"
   });
 });
 
+// Creating user
 router.post('/register', (req, res) => {
 
+  // Checks to see if user already existis
   User.findOne({
       email: req.body.email
     })
@@ -27,6 +27,7 @@ router.post('/register', (req, res) => {
         });
       } else {
 
+        // Creating the avatar image from gravatar api
         const avatar = gravatar.url(req.body.email, {
           s: '200',
           r: 'pg',
@@ -34,7 +35,6 @@ router.post('/register', (req, res) => {
         })
 
         // Creating user from form data
-
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
@@ -62,5 +62,27 @@ router.post('/register', (req, res) => {
     })
 
 })
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //Find user with mongoose email
+  User.findOne({email})
+    .then(user => {
+      if(!user) {
+        res.status(404).json({email: 'user not founnd'})
+      }
+      // Check password of user
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            //  Will add login to return a jwt token for authentication
+            res.json({msg: 'Success'})
+          } else {
+            res.status(400).json({password: 'Password incorrect'})
+          }
+        })
+    })
+});
 
 module.exports = router;
